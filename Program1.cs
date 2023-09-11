8c75c0eef44c980d12d0bafa65cca1a5ffca9b3a
Polyukhovich Andriy <144113422+Polyukhovich@users.noreply.github.com>
2023-09-11 17:16:53 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Кількість чисел у послідовності
+
+        // Створення генератора випадкових чисел з поточним часом як початковим значенням
+        Random random1 = new Random();
+        Console.WriteLine("Sequence for identical initial values:"); // Виводимо заголовок для послідовності з однаковими початковими значеннями
+        Console.WriteLine("Please enter the number of numbers you would like to receive:");
+        int count1 = Convert.ToInt32(Console.ReadLine());
+        GenerateAndDisplayRandomNumbers(random1, count1); // Викликаємо функцію для генерації та виведення послідовності
+
+        // Початкове значення для другого генератора випадкових чисел
+        int seed2 = 123;
+        Random random2 = new Random(seed2);
+        Console.WriteLine("\nSequence for different initial values:"); // Виводимо заголовок для послідовності з різними початковими значеннями
+
+        GenerateAndDisplayRandomNumbers(random2, count1); // Викликаємо функцію для генерації та виведення другої послідовності
+
+        // Програма завершується після виведення послідовностей
+    }
+
+    static void GenerateAndDisplayRandomNumbers(Random random, int length)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            long randomNumber = (long)random.Next(13, 132); // Генерує числа
+
+            Console.WriteLine($"element {i + 1}: {randomNumber}"); // Виводимо псевдовипадкове число разом з номером елемента
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the number of random numbers to generate: ");
+        if (int.TryParse(Console.ReadLine(), out int sequenceLength) && sequenceLength > 0)
+        {
+            Console.WriteLine($"Generating a cryptographically secure random sequence of {sequenceLength} numbers:");
+            // Створення об'єкта RNGCryptoServiceProvider
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                for (int i = 0; i < sequenceLength; i++)
+                {
+                    byte[] randomNumberBytes = new byte[4]; // Створюємо байтовий масив для зберігання генерованого числа
+                    // Генеруємо криптографічно стійке випадкове число
+                    rng.GetBytes(randomNumberBytes);
+                    // Перетворюємо байтовий масив в ціле число (int)
+                    int randomNumber = BitConverter.ToInt32(randomNumberBytes, 0);
+                    Console.WriteLine($"Element {i + 1}: {randomNumber}"); // Виводимо генероване число разом з номером елемента
+                }
+            }
+            // Завершуємо програму
+            Environment.Exit(0);
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a positive integer for the number of random numbers to generate.");
+        }
+
+        Console.ReadLine(); // Очікуємо, поки користувач натисне клавішу перед завершенням програми
+    }
+}
