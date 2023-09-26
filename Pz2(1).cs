20415ee44d0edc59d087668ff79f23599b7f8d06
Polyukhovich Andriy <144113422+Polyukhovich@users.noreply.github.com>
2023-09-26 10:04:04 +0300

    Add files via upload

---
+++ Pz2(1).cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the text-->");
+        string text = Console.ReadLine();
+        Console.WriteLine("Enter the key-->");
+        string key = Console.ReadLine();
+
+        // Encrypt the text
+        byte[] textBytes = Encoding.UTF8.GetBytes(text);
+        byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+
+        byte[] encryptedText = new byte[textBytes.Length];
+
+        for (int i = 0; i < textBytes.Length; i++)
+        {
+            encryptedText[i] = (byte)(textBytes[i] ^ keyBytes[i % keyBytes.Length]);
+        }
+
+        // Save encrypted text to a file
+        using (FileStream encryptStream = new FileStream("encrypted.dat", FileMode.OpenOrCreate))
+        {
+            encryptStream.Write(encryptedText, 0, encryptedText.Length);
+        }
+
+        Console.WriteLine("Text encrypted and written to encrypted.dat.");
+
+        // Decrypt the text
+        byte[] decryptedText;
+
+        using (FileStream decryptStream = new FileStream("encrypted.dat", FileMode.Open))
+        {
+            decryptedText = new byte[decryptStream.Length];
+            decryptStream.Read(decryptedText, 0, (int)decryptStream.Length);
+        }
+
+        byte[] originalText = new byte[decryptedText.Length];
+
+        for (int i = 0; i < decryptedText.Length; i++)
+        {
+            originalText[i] = (byte)(decryptedText[i] ^ keyBytes[i % keyBytes.Length]);
+        }
+
+        string decryptedTextStr = Encoding.UTF8.GetString(originalText);
+
+        Console.WriteLine("Decrypted Text: " + decryptedTextStr);
+    }
+}
---
+++ Pz2(2).cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class BruteforceDecryptor
+{
+    static void Main()
+    {
+        string encryptedFilePath = "encrypted.dat"; // Вкажіть шлях до зашифрованого файлу
+        string targetPhrase = "Mit21"; // Фраза, яку ми шукаємо
+        int passwordLength = 5; // Довжина пароля
+
+        byte[] encryptedData = File.ReadAllBytes(encryptedFilePath);
+        byte[] password = new byte[passwordLength];
+
+        Console.WriteLine("Decrypting...");
+
+        for (int i = 0; i <= encryptedData.Length - passwordLength; i++)
+        {
+            for (int j = 0; j < passwordLength; j++)
+            {
+                password[j] = (byte)(encryptedData[i + j] ^ (byte)targetPhrase[j]);
+            }
+
+            string decryptedText = DecryptText(password, encryptedData);
+
+            if (decryptedText.Contains(targetPhrase))
+            {
+                Console.WriteLine($"Password found: {Encoding.UTF8.GetString(password)}");
+                Console.WriteLine($"Decrypted Text: {decryptedText}");
+                return; // Завершити програму після знаходження пароля і тексту
+            }
+        }
+
+        Console.WriteLine("Password not found.");
+    }
+
+    static string DecryptText(byte[] password, byte[] encryptedData)
+    {
+        byte[] decryptedText = new byte[encryptedData.Length];
+
+        for (int i = 0; i < encryptedData.Length; i++)
+        {
+            decryptedText[i] = (byte)(encryptedData[i] ^ password[i % password.Length]);
+        }
+
+        return Encoding.UTF8.GetString(decryptedText);
+    }
+}
